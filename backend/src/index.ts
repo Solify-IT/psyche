ff17aec499cd8427fcd9c7a7a94f9bd07583a40f
Alberto Castañeda <albcastar@gmail.com>
2021-03-19 14:04:03 -0600

    chore: add typescript linter

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 
-
-
 const portNumber: number = 8000;
 const app: express.Application = express();
 
-
 const initServer = () => {
-    app.use(express.json());
-    app.use(cors());
-}
+  app.use(express.json());
+  app.use(cors());
+};
 
 app.listen(portNumber, async () => {
-    console.log("Backend server running on port " 
-    + portNumber)
+  console.log(`Backend server running on port ${
+    portNumber}`);
 
-    initServer();
+  initServer();
 });
