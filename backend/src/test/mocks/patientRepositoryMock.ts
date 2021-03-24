36e43d9aeef4074d5e34c3e7965e86c20975fc99
Alberto Castañeda <albcastar@gmail.com>
2021-03-23 22:41:22 -0600

    chore: fix ci file

--- backend/src/test/mocks/patientRepositoryMock.ts
+++ backend/src/test/mocks/patientRepositoryMock.ts
@@ -3,9 +3,11 @@ import { Patient } from 'domain/model';
 import { IPatientRepository } from 'app';
 
 export default class MockPatientRepository implements IPatientRepository {
+
   async findAll(): Promise<Patient[]> {
     const patients : Patient[] = [];
-    for (let index = 0; index < patientsTotal; index++) {
+
+    for (let index = 0; index < 5; index++) {
       patients.push(
         { name: `Test patient #${index}` },
       );
