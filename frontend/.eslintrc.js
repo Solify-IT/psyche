e7f9e7d3eea20f2635bed86fda5728c2cfc297e0
Alberto Castañeda <albcastar@gmail.com>
2021-03-24 16:54:08 -0600

    chore: update ci workflow to upload test coverage and lint verify

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -2,5 +2,9 @@ module.exports = {
     extends: ['airbnb-typescript'],
     parserOptions: {
         project: './tsconfig.json',
+    },
+    rules: {
+        "class-methods-use-this": "off",
     }
+    
 };
