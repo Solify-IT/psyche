00bf168211d315a9b8c4954cd0f0b9a54b6bc8c5
Alberto Castañeda <albcastar@gmail.com>
2021-04-05 00:52:31 -0500

    chore: add dotenv environment variable management

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import Router from './infraestructure/router/router';
 import Datastore from './infraestructure/datastore/datastore';
 import Registry from './registry';
 
-const portNumber: number = 8000;
+let portNumber = '8000';
 const app: express.Application = express();
 
 const initServer = () => {
@@ -20,10 +21,19 @@ const setupRoutes = () => {
   const router = new Router(app, registry.newAppController());
 };
 
-app.listen(portNumber, async () => {
-  console.log(`Backend server running on port ${
-    portNumber}`);
+const setupEnvironment = () => {
+  const result = dotenv.config();
+  if (result.error) {
+    console.error('An error ocurred attempting to open .env file. Make sure it is created properly on the project root.');
+    return;
+  }
+  portNumber = process.env.PORT || '8000';
+  console.log(result.parsed);
+};
 
+app.listen(process.env.PORT || 8000, async () => {
+  setupEnvironment();
   initServer();
   setupRoutes();
+  console.log('Backend service started.');
 });
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+console.log(`Server running with API URL: ${process.env.REACT_APP_API_URL}`);
 ReactDOM.render(
   <React.StrictMode>
     <App />
