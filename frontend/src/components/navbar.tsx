b73101976ac3e61f0ac6ae6ec791bfc696abd1fa
Ricardo Escobar <ricardoescobar@Ricardos-MacBook-Pro.local>
2021-04-07 12:11:48 -0500

    Implementacinnn inicial de estilo

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,17 +1,66 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+import MenuIcon from '@material-ui/icons/Menu';
+import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom';
 import '../App.css';
+import { makeStyles } from '@material-ui/core';
 
 function Navbar() {
-  return (
-    <nav style={{ margin: 10 }}>
-      <Link to="/" style={{ padding: 10 }}>
-        Home
-      </Link>
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+    navBar: {
+      background: '#C94B72',
+    },
+    button: {
+      color: '#FFFFFF',
+      textDecoration: 'none',
+    },
+    paper: {
+      marginRight: theme.spacing(2),
+    },
+    showBar: {
+      color: '#432918',
+      textDecoration: 'none',
+    },
+    logo: {
+      width: '120px',
+      height: 'auto',
+      paddingTop: theme.spacing(1),
+    },
+  }));
+  const classes = useStyles();
 
-      <Link to="/pacientes" style={{ padding: 10 }}>
-        Pacientes
-      </Link>
+  return (
+    <nav>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              <Link to="/">
+                <img src="/images/logo.png" alt="logo of Psyque" className={classes.logo} />
+              </Link>
+            </Typography>
+            <Link to="/" className={classes.button}><Button className={classes.button}>Opcion 1</Button></Link>
+            <Link to="/" className={classes.button}><Button className={classes.button}>Opcion 2</Button></Link>
+            <Link to="/" className={classes.button}><Button className={classes.button}>Opcion 3</Button></Link>
+          </Toolbar>
+        </AppBar>
+      </div>
     </nav>
   );
 }
