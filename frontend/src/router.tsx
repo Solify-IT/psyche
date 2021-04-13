0db00206c38cc2f45593a9a43c551df2e2506c46
Alberto Castañeda <albcastar@gmail.com>
2021-04-13 14:16:57 -0500

    fix: add browser env to eslint and remove redux

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 // import Home from './views/home';
-import PatientsList from './views/patients';
 import Login from './views/login';
 
 const AppRouter = () => (
   <div>
     <Route path="/" exact component={Login} />
-    <Route path="/pacientes" component={PatientsList} />
   </div>
 );
 
