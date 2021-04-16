c0b6206ff9d0e38a0e2f70415558def55ac70a42
Ricardo Escobar <ricardoescobar@Ricardos-MacBook-Pro.local>
2021-04-16 16:50:40 -0500

    subir forms

--- frontend/src/views/data.js
+++ frontend/src/views/data.js
@@ -1,17 +1,20 @@
 const data = [
   {
+    id: 'eb169f76-4cd9-4513-b673-87c5c7d27e02',
     label: 'Nombre del Paciente',
     type: 'Text',
     name: 'patientName',
     value: 'Ricardo',
   },
   {
+    id: 'eb169f76-4cd9-4513-b673-87c5c7d27e03',
     label: 'Apellidos del Paciente',
     type: 'Text',
     name: 'lastName',
     value: 'Escobar Gouyonnet',
   },
   {
+    id: 'eb169f76-4cd9-4513-b673-87c5c7d27e05',
     label: 'Edad',
     type: 'Number',
     name: 'age',
--- frontend/src/views/generateForm.tsx
+++ frontend/src/views/generateForm.tsx
@@ -4,6 +4,8 @@ import {
   makeStyles,
   Grid,
   Typography,
+  TextField,
+  Button,
 }
   from '@material-ui/core';
 import data from './data';
@@ -12,49 +14,119 @@ const useStyles = makeStyles((theme) => ({
   heroContent: {
     padding: theme.spacing(4, 0, 6),
   },
-}));
+  submit: {
 
-// interface CompleteField {
-//   label: string;
-//   type: string;
-//   name: string;
-//   value: string;
-// }
+  },
+}));
 
 function GenerateForm() {
   const classes = useStyles();
   const [fields, setFields] = useState(data);
 
-  function addField(field:any) {
-    if (field.name === 'dfsfs') {
-      setFields((prevFields) => [...prevFields, field]);
-    }
-  }
-
   useEffect(() => {
-    data.map(addField);
+    // Aqui vamos a mandar a llamar el endpoint de get
+    if (data !== []) {
+      setFields(data);
+    }
   }, []);
 
+  function getIndex(value:any, arr:any, prop:any) {
+    for (let i = 0; i < arr.length; i += 1) {
+      if (arr[i][prop] === value) {
+        return i;
+      }
+    }
+    return -1; // to handle the case where the value doesn't exist
+  }
+
+  const handleChange = (event: React.ChangeEvent<any>) => {
+    setFields((prevValues) => {
+      const aux = [...prevValues];
+      const index = getIndex(event.target.id, fields, 'id');
+      aux[index].value = event.target.value;
+      return aux;
+    });
+  };
+
   function createComponent(field:any) {
-    return (
-      <div>
-        <p>{field.label}</p>
-        <p>{field.type}</p>
-        <p>{field.name}</p>
-        <p>{field.value}</p>
-      </div>
-    );
+    switch (field.type) {
+      case 'Text':
+        return (
+          <Grid item xs={4}>
+            <TextField
+              key={field.id}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id={field.id}
+              label={field.label}
+              name={field.name}
+              value={field.value}
+              onChange={handleChange}
+            />
+          </Grid>
+        );
+      case 'Number':
+        return (
+          <Grid item xs={4}>
+            <TextField
+              key={field.id}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              type="number"
+              id={field.id}
+              label={field.label}
+              name={field.name}
+              value={parseInt(field.value, 10)}
+              onChange={handleChange}
+            />
+          </Grid>
+        );
+      default:
+        return (
+          <TextField
+            key={field.id}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            type="number"
+            id={field.id}
+            label={field.label}
+            name={field.name}
+            value={field.value}
+          />
+        );
+    }
   }
 
   return (
     <main>
       <div className={classes.heroContent}>
         <Typography variant="h2" align="center">
-          Visualizar Form
+          Editar Form
         </Typography>
         <Container>
-          <Grid container>
+          <Grid container spacing={3}>
             {fields.map(createComponent)}
+
+            <Grid container alignItems="center" justify="center" direction="row">
+              <Grid item>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  className={classes.submit}
+                >
+                  Modificar
+                </Button>
+              </Grid>
+
+            </Grid>
+
           </Grid>
         </Container>
       </div>
