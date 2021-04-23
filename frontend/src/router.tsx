c790677c9781cc32aaf51d11e328f53c2a6202a4
Doménica Rentería Berrospe <domerent@MacBook-Pro-de-Domenica.local>
2021-04-23 00:24:16 -0500

    dashboard

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -7,6 +7,7 @@ import Home from './views/Dashboard/home';
 import Form from './views/form';
 import GenerateForm from './views/generateForm';
 import DashboardArea from './views/Dashboard/dashboardArea';
+import DasboarPsic from './views/Dashboard/dashboardAsPsic';
 
 const AppRouter = () => (
   <div>
@@ -17,6 +18,7 @@ const AppRouter = () => (
     <PrivateRoute path="/patients" exact component={PatientsList} />
     <PrivateRoute path="/home" exact component={Home} />
     <PrivateRoute path="/dashboard-area" exact component={DashboardArea} />
+    <PrivateRoute path="/dashboard-as-psic" exact component={DasboarPsic} />
   </div>
 );
 
