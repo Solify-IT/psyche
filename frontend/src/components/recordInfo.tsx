e26d10154f251eed91aa3e398798ec4dc8c8ffe4
Ricardo Escobar <ricardoescobar@Ricardos-MacBook-Pro.local>
2021-04-29 15:07:42 -0500

    Change date to string

--- frontend/src/components/recordInfo.tsx
+++ frontend/src/components/recordInfo.tsx
@@ -138,7 +138,7 @@ function RecordInfo(props: RecordInfoProps) {
         <Grid container justify="space-between" spacing={2} className={classes.patientSectionRow}>
           <RecordInfoSlot label="Nombre" value={patient.name} />
           <RecordInfoSlot label="Apellidos" value={patient.lastName} />
-          <RecordInfoSlot label="Edad" value={calculateAge(patient.birthDate).toString()} />
+          <RecordInfoSlot label="Edad" value={patient.birthDate} />
         </Grid>
         <Grid container justify="space-between" spacing={2} className={classes.patientSectionRow}>
           <RecordInfoSlot label="Genero" value={patient.gender} />
