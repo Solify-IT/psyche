1a9814b95a59e28b9844a880f4b5a68ab75b4927
scarlosro <33816613+scarlosro@users.noreply.github.com>
2021-06-01 22:52:38 -0500

    feat(user): admin changes password (#77)
    * Añadir end point
    
    * Fix
    
    * Front-end
    
    * End user story
    
    * Fix
    
    * fix lint
    
    Co-authored-by: Carlos Sánchez <carlossanchez@MacBook-Pro-de-Carlos.local>

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -49,6 +49,7 @@ import UpdateUser from './views/Users/updateUser';
 import ChangePassword from './views/Users/changePassword';
 import NotFound from './components/NotFound';
 import UserRole from './fixtures/roles';
+import ChangePasswordAdmin from './views/Users/changePasswordAdmin';
 
 const AppRouter = () => (
   <Switch>
@@ -88,6 +89,7 @@ const AppRouter = () => (
     <PrivateRoute path="/update-form/:id" exact component={UpdateForm} />
     <PrivateRoute path="/view-patients" exact component={ViewPatients} />¿
     <PrivateRoute path="/update-form/:id" exact component={UpdateForm} roles={[UserRole.Administrador]} />
+    <PrivateRoute path="/change-password/:id" exact component={ChangePasswordAdmin} roles={[UserRole.Administrador]} />
     <Route component={NotFound} />
   </Switch>
 );
---
+++ frontend/src/views/Users/changePasswordAdmin.tsx
@@ -0,0 +1,144 @@
+import React, { useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import {
+  makeStyles,
+  Grid,
+  TextField,
+  Paper,
+  Button,
+}
+  from '@material-ui/core';
+import { toast } from 'react-toastify';
+import { changePasswordAdmin } from 'src/api/user';
+import LoadingSpinner from 'src/components/loadingSpinner';
+import User from 'src/interfaces/user';
+import ContentTitle from 'src/components/contentTitle';
+import MainContent from 'src/components/mainContent';
+
+const useStyles = makeStyles((theme) => ({
+  heroContent: {
+    padding: theme.spacing(4, 0, 6),
+  },
+  paper: {
+    marginTop: '50px',
+  },
+  submit: {
+    textAlign: 'center',
+    marginBottom: '20px',
+  },
+  form: {
+    marginTop: '35px',
+  },
+}));
+
+function ChangePasswordAdmin() {
+  const classes = useStyles();
+  const history = useHistory();
+  const { id } : any = useParams();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [fields, setFields] = useState<any>({
+    password: '',
+    newPassword: '',
+    confirmNewPassword: '',
+  });
+
+  const handleChange = (event: React.ChangeEvent<any>) => {
+    setFields({ ...fields, [event.target.name]: event.target.value });
+  };
+
+  function validPassword(password: string) : boolean {
+    const strongRegex = new RegExp(
+      '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*.])(?=.{5,})',
+    );
+    return strongRegex.test(password);
+  }
+
+  async function handleSubmit() {
+    if (fields.newPassword !== fields.confirmNewPassword) {
+      toast.warning('¡Las contraseñas no coinciden!');
+    } else if (!validPassword(fields.newPassword)) {
+      toast.warning('La contraseña debe ser mayor a 5 carácteres y contener al menos una minúscula, una mayúscula, un número y un carácter especial');
+    } else {
+      setLoading(true);
+      try {
+        const user : User = await changePasswordAdmin(id, fields.confirmNewPassword);
+        console.log(user);
+        toast.success('¡Actualización completada! 😃');
+        history.push('/view-users');
+      } catch (error) {
+        if (error.response && error.response.status === 404) {
+          toast.error('No se encontró un usuario con los datos proporcionados.');
+        } else {
+          toast.error('Ocurrió un error de conexión.');
+        }
+      } finally {
+        setLoading(false);
+      }
+    }
+  }
+
+  return (
+    <MainContent>
+      <ContentTitle text="Cambiar Contraseña" />
+      <Grid
+        container
+        justify="center"
+        spacing={2}
+      >
+        <Grid
+          item
+          xs={8}
+          component={Paper}
+          className={classes.paper}
+          elevation={6}
+          square
+        >
+          <Grid className={classes.form} container justify="center" alignItems="center" spacing={3}>
+            <Grid item xs={8} sm={7}>
+              <TextField
+                variant="outlined"
+                type="password"
+                inputProps={{ minLength: 8 }}
+                required
+                fullWidth
+                id="newPassword"
+                value={fields.newPassword}
+                label="Nueva Contraseña"
+                name="newPassword"
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={8} sm={7}>
+              <TextField
+                variant="outlined"
+                type="password"
+                inputProps={{ minLength: 8 }}
+                required
+                fullWidth
+                id="confirmNewPassword"
+                value={fields.confirmNewPassword}
+                label="Confirmar Contraseña"
+                name="confirmNewPassword"
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={8} className={classes.submit}>
+              { loading ? <LoadingSpinner /> : (
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  disabled={fields.newPassword === '' || fields.confirmNewPassword === ''}
+                  onClick={handleSubmit}
+                >
+                  Actualizar
+                </Button>
+              )}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </MainContent>
+  );
+}
+export default ChangePasswordAdmin;
